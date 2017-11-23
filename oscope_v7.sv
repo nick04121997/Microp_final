611c95f52ce2e4730e24c135709139bc94ec5ae1
Nicholas Draper <nicholasdraper@WL-211-236.WPA.Claremont.Edu>
2017-11-22 20:14:25 -0800

    changed adc_com to account for measurements

--- oscope_v7.sv
+++ oscope_v7.sv
@@ -51,7 +51,7 @@ module adc_com(input logic osc_clk,
 	statetype state, nexstate;
 
 	// temp register
-	logic [15:0] temp_data;
+	logic [11:0] temp_data;
 	// counter for adc_clk
 	logic [24:0] counter;
 
@@ -83,24 +83,28 @@ module adc_com(input logic osc_clk,
 		begin
 			state <= nexstate;
 			case (state)
-				S0:		; // do nothing
-				S1:		temp_data[15] <= adc_data;
-				S2:		temp_data[14] <= adc_data;
-				S3:		temp_data[13] <= adc_data;
-				S4:		temp_data[12] <= adc_data;
-				S5:		temp_data[11] <= adc_data;
-				S6:		temp_data[10] <= adc_data;
-				S7:		temp_data[9] <= adc_data;
-				S8:		temp_data[8] <= adc_data;
-				S9:		temp_data[7] <= adc_data;
-				S10:		temp_data[6] <= adc_data;
-				S11:		temp_data[5] <= adc_data;
-				S12:		temp_data[4] <= adc_data;
-				S13:		temp_data[3] <= adc_data;
-				S14:		temp_data[2] <= adc_data;
-				S15:		temp_data[1] <= adc_data;
-				S16:		temp_data[0] <= adc_data;
-				S17:		; // do nothing
+				// do nothing
+				S0:		; 
+				// conv goes low, do nothing
+				S1:		; 
+				// junk cycle no data, do nothing
+				S2:		;
+				// MSB D11
+				S3:		temp_data[11] <= adc_data;
+				S4:		temp_data[10] <= adc_data;
+				S5:		temp_data[9] <= adc_data;
+				S6:		temp_data[8] <= adc_data;
+				S7:		temp_data[7] <= adc_data;
+				S8:		temp_data[6] <= adc_data;
+				S9:		temp_data[5] <= adc_data;
+				S10:		temp_data[4] <= adc_data;
+				S11:		temp_data[3] <= adc_data;
+				S12:		temp_data[2] <= adc_data;
+				S13:		temp_data[1] <= adc_data;
+				// LSB D0
+				S14:		temp_data[0] <= adc_data;
+				// Raises the conv pin and write enable no data to collect
+				S15:	;		
 				default:	; // do nothing
 			endcase
 		end
@@ -111,112 +115,102 @@ module adc_com(input logic osc_clk,
 	always_comb
 	begin
 		case (state)
+			// default conv is high and no writing
 			S0: 		begin
 							adc_conv = 1;
 							write_enable = 0;
 							nexstate = S1;
 						end
-
+			// first time conv goes low
 			S1:			begin
 							adc_conv = 0;
 							write_enable = 0;
 							nexstate = S2;
 						end
-
+			// junk cycle no data comes in yet
 			S2:			begin
 							nexstate = S3;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D11 (MSB) comes in at this point
 			S3:			begin
 							nexstate = S4;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D10		
 			S4:			begin 
 							nexstate = S5;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D9	
 			S5: 		begin
 							nexstate = S6;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D8
 			S6:			begin
 							nexstate = S7;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D7
 			S7:			begin
 							nexstate = S8;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D6
 			S8:			begin
 							nexstate = S9;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D5
 			S9:			begin
 							nexstate = S10;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D4
 			S10: 		begin
 							nexstate = S11;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D3
 			S11:		begin
 							nexstate = S12;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D2
 			S12:		begin
 							nexstate = S13;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D1
 			S13:		begin
 							nexstate = S14;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// D0 (LSB) comes in
 			S14:		begin
 							nexstate = S15;
 							adc_conv = 0;
 							write_enable = 0;
 						end
-
+			// Conv goes high and so do does write enable
+			// then we repeat states
 			S15: 		begin
-							nexstate = S16;
-							adc_conv = 0;
-							write_enable = 0;
-						end
-
-			S16:		begin
-							nexstate = S17;
-							adc_conv = 0;
-							write_enable = 0;
-						end
-
-			S17:		begin
 							nexstate = S0;
-							write_enable = 1;
 							adc_conv = 1;
+							write_enable = 1;
 						end
 
 			default:	begin
@@ -224,12 +218,11 @@ module adc_com(input logic osc_clk,
 							adc_conv = 1;
 							write_enable = 0;
 						end
-
 		endcase // state
 	end
 
-	assign adc_clk = counter[22];
-	assign write_data = temp_data[12:5];
+	assign adc_clk = counter[5];
+	assign write_data = temp_data[11:4];
 	assign led = counter[24];
 
 endmodule
